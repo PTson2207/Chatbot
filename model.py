75f1a0314009f9bdbc182908a416e4c5ace437a6
PTson2207 <sonphukl@gmail.com>
2021-09-15 15:16:55 +0700

    update model

---
+++ model.py
@@ -0,0 +1,131 @@
+from collections import Counter
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import string
+
+
+# Create Attention for transformer
+def scaled_dot_product_attention(query, key, value, mask):
+    matmul_qk = tf.matmul(query, key, transpose_b=True)
+    depth = tf.cast(tf.shape(key)[-1], tf.float32)
+    logits = matmul_qk / tf.math.sqrt(depth)
+    if mask is not None:
+        logits += (mask * -1e9)
+    
+    attention_weights = tf.nn.softmax(logits, axis=-1)
+    output = tf.matmul(attention_weights, value)
+    return output
+
+# Mulithead attention
+class MultiHeadAttention(tf.keras.layers.Layer):
+
+    def __init__(self, d_model, num_heads, name="multi_head_attention"):
+        super(MultiHeadAttention, self).__init__(name=name)
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        assert d_model % self.num_heads == 0
+        
+        self.depth = d_model // self.num_heads
+        self.query_dense = tf.keras.layers.Dense(units=d_model)
+        self.key_dense = tf.keras.layers.Dense(units=d_model)
+        self.value_dense = tf.keras.layers.Dense(units=d_model)
+
+        self.dense = tf.keras.layers.Dense(units=d_model)
+
+    def get_config(self):
+        config = super(MultiHeadAttention, self).get_config()
+        config.update({
+            'num_heads': self.num_heads,
+            'd_model': self.d_model,
+        })
+        return config 
+    
+    def split_heads(self, inputs, batch_size):
+        inputs = tf.keras.layers.Lambda(lambda inputs: tf.reshape(
+            inputs, shape=(batch_size, -1, self.num_heads, self.depth)
+        ))(inputs)
+        return tf.keras.layers.Lambda(lambda inputs: tf.transpose(
+            inputs, perm=[0, 2, 1, 3]))(inputs)
+
+    def call(self, inputs, **kwargs):
+        query, key, value, mask = inputs['query'], inputs['key'], inputs['value'], inputs['mask']
+        batch_size = tf.shape(query)[0]
+
+        #linear layer
+        query = self.query_dense(query)
+        key = self.key_dense(key)
+        value = self.value_dense(value)
+
+        #split heads
+        query = self.split_heads(query, batch_size)
+        key = self.split_heads(key, batch_size)
+        value = self.split_heads(value, batch_size)
+
+        #scale dot-product attention
+        scaled_attention = scaled_dot_product_attention(query, key, value, mask)
+        scaled_attention = tf.keras.layers.Lambda(lambda scaled_attention: tf.transpose(
+            scaled_attention, perm=[0, 2, 1, 3]))(scaled_attention)
+
+        #concatention of heads
+        concat_attention = tf.keras.layers.Lambda(lambda scaled_attention: tf.reshape(
+            scaled_attention, (batch_size, -1, self.d_model)))(scaled_attention)
+
+        #final linear
+        outputs = self.dense(concat_attention)
+
+        return outputs
+    
+# Create masked padding
+def create_padding_mask(x):
+    mask = tf.cast(tf.math.equal(x, 0), tf.float32)
+    return mask[:, tf.newaxis, tf.newaxis, :]
+
+# Create look-ahead mask
+def create_look_ahead_mask(x):
+    seq_len = tf.shape(x)[1]
+    look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((seq_len, seq_len)), -1, 0)
+    padding_mask = create_padding_mask(x)
+    return tf.maximum(look_ahead_mask, padding_mask)
+
+# Positional encoding
+class PositionalEncoding(tf.keras.layers.Layer):
+
+    def __init__(self, position, d_model):
+        super(PositionalEncoding, self).__init__()
+        self.pos_encoding = self.positional_encoding(position, d_model)
+
+    def get_config(self):
+        config = super(PositionalEncoding, self).get_config()
+        config.update({
+            'position': self.position,
+            'd_model': self.d_model,
+
+        })
+        return config
+
+    def get_angles(self, position, i, d_model):
+        angles = 1 / tf.pow(10000, (2 * (i // 2)) / tf.cast(d_model, tf.float32))
+        return position * angles
+
+    def positional_encoding(self, position, d_model):
+        angle_rads = self.get_angles(
+            position=tf.range(position, dtype=tf.float32)[:, tf.newaxis],
+            i=tf.range(d_model, dtype=tf.float32)[tf.newaxis, :],
+            d_model=d_model)
+        # hàm sin
+        sines = tf.math.sin(angle_rads[:, 0::2])
+        # hàm cosin 
+        cosines = tf.math.cos(angle_rads[:, 1::2])
+
+        pos_encoding = tf.concat([sines, cosines], axis=-1)
+        pos_encoding = pos_encoding[tf.newaxis, ...]
+        return tf.cast(pos_encoding, tf.float32)
+
+    def call(self, inputs, **kwargs):
+        return inputs + self.pos_encoding[:, :tf.shape(inputs)[1], :]
