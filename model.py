571cc7bc04e42a6e2e05164386cedae7e7da7a21
PTson2207 <sonphukl@gmail.com>
2021-09-17 00:15:30 +0700

    update encoder block

--- model.py
+++ model.py
@@ -154,3 +154,34 @@ def encoder_layer(units, d_model, num_heads, dropout, name="encoder_layer"):
 
     return tf.keras.Model(
         inputs=[inputs, padding_mask], outputs=outputs, name=name)
+
+# Encoder
+def encoder(vocab_size,
+            num_layers,
+            units,
+            d_model,
+            num_heads,
+            dropout,
+            name="encoder"):
+    inputs = tf.keras.Input(shape=(None,), name="inputs")
+    padding_mask = tf.keras.Input(shape=(1, 1, None), name="padding_mask")
+
+    embeddings = tf.keras.layers.Embedding(vocab_size, d_model)(inputs)
+    embeddings *= tf.keras.layers.Lambda(lambda d_model: tf.math.sqrt(tf.cast(d_model, tf.float32)))(d_model)
+    embeddings = PositionalEncoding(vocab_size, d_model)(embeddings)
+
+    outputs = tf.keras.layers.Dropout(rate=dropout)(embeddings)
+
+    for i in range(num_layers):
+        outputs = encoder_layer(
+            units=units,
+            d_model=d_model,
+            num_heads=num_heads,
+            dropout=dropout,
+            name="encoder_layer_{}".format(i),
+        )([outputs, padding_mask])
+
+    return tf.keras.Model(
+        inputs=[inputs, padding_mask], outputs=outputs, name=name)
+
+# decode layers
