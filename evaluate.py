e2a3ec68d2d6f51806e1d25f72287002e05c3c21
PTson2207 <sonphukl@gmail.com>
2021-09-17 13:41:33 +0700

    update evaluation

---
+++ evaluate.py
@@ -0,0 +1,42 @@
+from collections import Counter
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import string
+
+
+def loss_function(y_true, y_pred):
+    y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')(y_true, y_pred)
+
+    mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+    loss = tf.multiply(loss, mask)
+
+    return tf.reduce_mean(loss)
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+
+    def __init__(self, d_model, warmup_steps=4000):
+        super(CustomSchedule, self).__init__()
+
+        self.d_model = tf.constant(d_model, dtype=tf.float32)
+        self.warmup_steps = warmup_steps
+
+    def get_config(self):
+        return {"d_model": self.d_model, "warmup_steps": self.warmup_steps}
+
+    def __call__(self, step):
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
+
+        return tf.math.multiply(tf.math.rsqrt(self.d_model), tf.math.minimum(arg1, arg2))
+
+def accuracy(y_true, y_pred):
+    y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+    return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
